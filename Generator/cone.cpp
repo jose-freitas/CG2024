1336c376c430c908161e324f63b83273a6b0c215
Inês Castro <73346645+graphics-ins@users.noreply.github.com>
2024-03-03 01:56:46 +0000

    - a testar

--- Generator/cone.cpp
+++ Generator/cone.cpp
@@ -7,61 +7,89 @@
 #include <GL/glut.h>
 #endif
 
+#include <vector>
+
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <string>
+#include <bitset>
 
-void changeSize(int w, int h) {
+int timebase;
+float frame;
+int verticeCount;
+GLuint vertices, verticeCount;
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window with zero width).
-	if(h == 0)
-		h = 1;
+float alfa = 0.0f, beta = 0.0f, radius = 5.0f;
+float camX, camY, camZ;
 
-	// compute window's aspect ratio 
-	float ratio = w * 1.0 / h;
+void spherical2Cartesian() {
 
-	// Set the projection matrix as current
-	glMatrixMode(GL_PROJECTION);
-
-	// Load Identity Matrix
-	glLoadIdentity();
-	
-	// Set the viewport to be the entire window
-    glViewport(0, 0, w, h);
-
-	// Set perspective
-	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
-
-	// return to the model view matrix mode
-	glMatrixMode(GL_MODELVIEW);
+	camX = radius * cos(beta) * sin(alfa);
+	camY = radius * sin(beta);
+	camZ = radius * cos(beta) * cos(alfa);
 }
 
+void prepareCone(float radius, float height, int slices) {
 
-void drawCone(float radius, float height, int slices) {
+    vector<float> p;
+    int i;
+
 
     int delta = ((2*M_PI)/slices);
 
-    glPolygoMode (GL_FRONT_AND_BACK, GL_LINE);
-
     glBegin(GL_TRIANGLES);
+
+
     for(int i = 0; i<slices; i++)
     {
         float alfa = delta * i;
         float nextalfa = delta * (i+1);
 
-        // Base
-        glVertex3f((radius * sin(alfa)) ,0 , (radius * cos(alfa)));
-        glVertex3f((radius * sin(nextalfa)),0 , (radius * cos(nextalfa)));
-        glVertex3f(0 ,0 ,0 );
+        // vérice do topo
+        p.push_back(0.0f);
+        p.push_back(height);
+        p.push_back(0.0f);
 
-        // Lateral
-        glVertex3f((radius * sin(alfa)), height, (radius * cos(alfa)));
-        glVertex3f((radius * sin(alfa)) ,0 , (radius * cos(alfa)));
-        glVertex3f((radius * sin(nextalfa)),0 , (radius * cos(nextalfa)));
+        // bottom
+        p.push_back((radius * sin(alfa)));
+        p.push_back(0.0f);
+        p.push_back((radius * cos(alfa)));
 
+        p.push_back(0);
+        p.push_back(0);
+        p.push_back(0);
+
+        p.push_back((radius * sin(nextalfa)));
+        p.push_back((0));
+        p.push_back((radius * cos(nextalfa)));
+
+        //body
+        p.push_back((radius * sin(alfa)));
+        p.push_back((height));
+        p.push_back((radius * cos(alfa)));
+
+        p.push_back(0.0f);
+        p.push_back(height);
+        p.push_back(0.0f);
+
+        p.push_back((radius * sin(nextalfa)));
+        p.push_back((0));
+        p.push_back((radius * cos(nextalfa)));
     }
     glEnd();
+    
 
+	verticeCount = p.size() / 3;
+
+	// criar o VBO
+    glGenBuffers(1, &vertices);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertices);
+ 	glBufferData(GL_ARRAY_BUFFER, 
+                    sizeof(float) * p.size(), // tamanho do vector em bytes
+ 	                p.data(), // os dados do array associado ao vector
+ 	                GL_STATIC_DRAW); // indicativo da utilização (estático e para desenho)
+	
 }
 
 void renderScene(void) {
@@ -71,12 +99,30 @@ void renderScene(void) {
 
 	// set the camera
 	glLoadIdentity();
-	gluLookAt(cameraX,cameraY,cameraZ,
-		      0.0,0.0,0.0,
-			  0.0f,1.0f,0.0f);
+	gluLookAt(camX, camY, camZ,
+		0.0, 0.0, 0.0,
+		0.0f, 1.0f, 0.0f);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vertices);
+ 	glVertexPointer(3, GL_FLOAT, 0, 0);
+ 	glDrawArrays(GL_TRIANGLES, 0, verticeCount);
 
-	drawCylinder(1,2,10);
+	prepareCone(1,2,10);
+	//cylinder(1,2,10);
 
-	// End of frame
+    frame++;
+	float fps = 0;
+    int time = glutGet(GLUT_ELAPSED_TIME);
+    if (time - timebase > 1000) {
+        fps = frame*1000.0/(time-timebase);
+        timebase = time;
+        frame = 0;
+    }
+
+    char buffer[20]; 
+    sprintf(buffer, "%.2f", fps);
+	glutSetWindowTitle(buffer);
+
+    // End of frame
 	glutSwapBuffers();
 }
