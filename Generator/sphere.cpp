1d6f56f41978e0c2b50972dcc7a9a528f98dd9fc
jose-freitas <a96140@alunos.uminho.pt>
2024-03-05 12:54:00 +0000

    esfera feita

--- Generator/sphere.cpp
+++ Generator/sphere.cpp
@@ -1,16 +1,11 @@
-#include <GL/glut.h>
-#include <vector>
-#include <cmath>
-
 #define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <vector>
 
-using namespace std;
-
-int verticeCount;
-GLuint vertices;
 
 void prepareSphere(float radius, int slices, int stacks) {
-    vector<float> p;
+    std::vector<float> vertices;
 
     float deltaPhi = (float)(M_PI) / stacks;
     float deltaTheta = (float)(2 * M_PI) / slices;
@@ -24,70 +19,30 @@ void prepareSphere(float radius, int slices, int stacks) {
             float nextTheta = (j + 1) * deltaTheta;
 
             // Primeiro triângulo do quadrilátero
-            p.push_back(radius * sin(phi) * cos(theta));
-            p.push_back(radius * cos(phi));
-            p.push_back(radius * sin(phi) * sin(theta));
+            vertices.push_back(radius * sin(phi) * cos(theta));
+            vertices.push_back(radius * cos(phi));
+            vertices.push_back(radius * sin(phi) * sin(theta));
 
-            p.push_back(radius * sin(phi) * cos(nextTheta));
-            p.push_back(radius * cos(phi));
-            p.push_back(radius * sin(phi) * sin(nextTheta));
+            vertices.push_back(radius * sin(phi) * cos(nextTheta));
+            vertices.push_back(radius * cos(phi));
+            vertices.push_back(radius * sin(phi) * sin(nextTheta));
 
-            p.push_back(radius * sin(nextPhi) * cos(theta));
-            p.push_back(radius * cos(nextPhi));
-            p.push_back(radius * sin(nextPhi) * sin(theta));
+            vertices.push_back(radius * sin(nextPhi) * cos(theta));
+            vertices.push_back(radius * cos(nextPhi));
+            vertices.push_back(radius * sin(nextPhi) * sin(theta));
 
             // Segundo triângulo do quadrilátero
-            p.push_back(radius * sin(nextPhi) * cos(theta));
-            p.push_back(radius * cos(nextPhi));
-            p.push_back(radius * sin(nextPhi) * sin(theta));
+            vertices.push_back(radius * sin(nextPhi) * cos(theta));
+            vertices.push_back(radius * cos(nextPhi));
+            vertices.push_back(radius * sin(nextPhi) * sin(theta));
 
-            p.push_back(radius * sin(phi) * cos(nextTheta));
-            p.push_back(radius * cos(phi));
-            p.push_back(radius * sin(phi) * sin(nextTheta));
+            vertices.push_back(radius * sin(phi) * cos(nextTheta));
+            vertices.push_back(radius * cos(phi));
+            vertices.push_back(radius * sin(phi) * sin(nextTheta));
 
-            p.push_back(radius * sin(nextPhi) * cos(nextTheta));
-            p.push_back(radius * cos(nextPhi));
-            p.push_back(radius * sin(nextPhi) * sin(nextTheta));
+            vertices.push_back(radius * sin(nextPhi) * cos(nextTheta));
+            vertices.push_back(radius * cos(nextPhi));
+            vertices.push_back(radius * sin(nextPhi) * sin(nextTheta));
         }
     }
-
-    verticeCount = p.size() / 3;
-    // criar o VBO
-    glGenBuffers(1, &vertices);
-    glBindBuffer(GL_ARRAY_BUFFER, vertices);
-    glBufferData(GL_ARRAY_BUFFER,
-                 sizeof(float) * p.size(), 
-                 p.data(),                 
-                 GL_STATIC_DRAW);          
 }
-
-void renderScene(void) {
-    // clear buffers
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // set the camera
-    glLoadIdentity();
-    gluLookAt(5.0f, 5.0f, 5.0f,
-              0.0, 0.0, 0.0,
-              0.0f, 1.0f, 0.0f);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vertices);
-    glVertexPointer(3, GL_FLOAT, 0, 0);
-    glDrawArrays(GL_TRIANGLES, 0, verticeCount);
-
-    // End of frame
-    glutSwapBuffers();
-}
-
-/*int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow("Sphere Example");
-    glEnable(GL_DEPTH_TEST);
-    glutDisplayFunc(renderScene);
-
-    prepareSphere(1.0f, 20, 20);
-
-    glutMainLoop();
-    return 0;
-}*/
